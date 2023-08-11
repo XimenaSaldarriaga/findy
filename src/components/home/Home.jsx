bcc4d069436f12c64939861812b57f187b66cd68
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-11 11:55:20 -0500

    create home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './home.scss'
+import logo from '../../assets/logo.png'
+import heart from '../../assets/heart.png'
+import messages from '../../assets/messages.png'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+        <div className='home__images'>
+            <img src={logo} alt="" />
+            <div className='home__vectors'>
+                <img className='home__vector' src={heart} alt="" />
+                <img className='home__vector' src={messages} alt="" />
+            </div>
+        </div>
+
+        <div className='home__yourStory'>
+            <input className='home__url' type="url" />
+            <span className='home__span'>Your Story</span>
+        </div>
+
+        </div>
+    )
 }
 
 export default Home
---
+++ src/components/postUser/PostUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostUser = () => {
+  return (
+    <div>Postuser</div>
+  )
+}
+
+export default PostUser
