9ad5b50aa59a76c1c00b7fea5d8bcf6f05c54c0d
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-14 13:46:12 -0500

    create footer

--- src/components/routes/Router.jsx
+++ src/components/routes/Router.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../home/Home'
-import PostUser from '../postUser/Postuser'
-import Profile from '../profile/Profile'
-import Login from '../login/Login'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../home/Home';
+import PostUser from '../postUser/Postuser';
+import Profile from '../profile/Profile';
+import Login from '../login/Login';
 
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/post" element={<PostUser />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home/:id" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/post" element={<PostUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
