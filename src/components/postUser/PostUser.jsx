28a7f1a389f40fd0a38d6cb4eeda5e9c7eb3d29a
Neis Rosado <neis.study@gmail.com>
2023-08-17 15:03:01 -0500

    Add functionalities in PostUser and change route post

--- src/components/postUser/PostUser.jsx
+++ src/components/postUser/PostUser.jsx
@@ -1,68 +1,116 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import heart from '../../assets/heart.png';
+import comment from '../../assets/comment.png';
+import send from '../../assets/send.png';
+import white from '../../assets/arrow-white.png';
+import { URL_POSTS, URL_USERS } from '../../services/data'
 import './postUser.scss'
-import heart from '../../assets/heart.png'
-import comment from '../../assets/comment.png'
-import send from '../../assets/send.png'
-import white from '../../assets/arrow-white.png'
-import { Link } from 'react-router-dom'
 
 const PostUser = () => {
+  const { postId } = useParams(); 
+  const [post, setPost] = useState(null);
+  const [user, setUser] = useState(null);
 
-  const userId = 'yourUserId';
+  useEffect(() => {
+    const fetchPostDetails = async () => {
+      try {
+ 
+        const postResponse = await axios.get(`${URL_POSTS}/${postId}`);
+        const postData = postResponse.data;
+        setPost(postData);
+
+        console.log(postData);
+
+       
+        const userResponse = await axios.get(`${URL_USERS}/${postData.userId}`);
+        const userData = userResponse.data;
+        setUser(userData);
+
+        console.log(userData);
+
+      } catch (error) {
+        console.error('Error fetching post details:', error);
+      }
+    };
+
+    fetchPostDetails();
+  }, [postId]);
+
+  if (!post || !user) {
+    return <div>Loading...</div>;
+  }
+
+  const isYouTubeLink = (url) => url.includes('youtube.com');
+  const getVideoIdFromUrl = (url) => {
+    const parts = url.split('/');
+    return parts[parts.length - 1];
+  };
+
 
   return (
-    <div className='post'>
-      <img className='post__image' src="https://s3-alpha-sig.figma.com/img/e16a/f17a/4805fb0a220637377cce1952fd4f4a39?Expires=1693180800&Signature=TIV6UK-wjy08selUQQsWxhJTU4O731UP8DrGCjlHxKfLQW49RMeiMLWM-vuQFZdhb0Vx~udYwPsEIFxoYXjEpb90REvuqtA9RL52gBd3hrpYhg5WID2H6USr20Ia6r3dcmiFFNESLHoZKtaQeCsPhy8pSJCmYcR99gU-bZi-zYqWsaZXY1zv9oZeCVugEpj52hWmpI-IdXqrBKrLjMcXx9o7qouAwEVz7xOTiIJENbgsZ9YnH2CiifsQpjyzDPZziU5FvMYct1Gschb9ElY8dj-Gc4O~1HEvYrLTky9iNwaIBvjnMZCco2Qnhf~YkldiWvH~wN6Qoq55~T9ZURKfuw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
-      <Link to={`/home/${userId}`}>
+        <div className='post'>
+          {isYouTubeLink(post.content) ? (
+        <div className='post__videoContainer'>
+          <iframe
+            className='post__video'
+            title={post.caption}
+            src={`https://www.youtube.com/embed/${getVideoIdFromUrl(post.content)}`}
+            frameBorder='0'
+            allowFullScreen
+          />
+        </div>
+      ) : (
+        <img className='post__image' src={post.content} alt={post.caption} />
+      )}
+      <Link to={`/home/${user.id}`}>
         <img className='post__arrow' src={white} alt="" />
       </Link>
-
-      <div className='post__all'>
-
-        <div className='post__info'>
-
-          <div className='post__user-name'>
-            <input className='post__input' type="url" />
-            <p className='post__name'>Jennie Kim</p>
+          <div className='post__all'>
+    
+            <div className='post__info'>
+    
+              <div className='post__user-name'>
+              <img className='post__input' src={user.avatar} alt={user.username} />
+            <p className='post__name'>{user.username}</p>
+              </div>
+    
+              <div className='post__quantity'>
+    
+              <div className='post__div'>
+                <img className='post__icons' src={heart} alt="" />
+                <p>{post.likes}K </p>
+              </div>
+              <div className='post__div'>
+                <img className='post__icons' src={comment} alt="" />
+                <p>54K</p>
+              </div>
+              <div className='post__div'>
+                <img className='post__icons' src={send} alt="" />
+                <p>2K</p>
+              </div>
+    
+              </div>
+    
+            </div>
+    
+            <div className='post__paragraph'>
+            <p>{post.caption}</p>
+            </div>
+    
+            <div className='post__comment'>
+              <input className='post__commentUser' type="url" />
+              <input className='post__commentMessage' type="text" placeholder='Write comment as username...' />
+            </div>
+    
           </div>
-
-          <div className='post__quantity'>
-            
-          <div className='post__div'>
-            <img className='post__icons' src={heart} alt="" />
-            <p>108K</p>
-          </div>
-          <div className='post__div'>
-            <img className='post__icons' src={comment} alt="" />
-            <p>54K</p>
-          </div>
-          <div className='post__div'>
-            <img className='post__icons' src={send} alt="" />
-            <p>2K</p>
-          </div>
-
-          </div>
-
+    
+    
+    
         </div>
-
-        <div className='post__paragraph'>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat provident aperiam laboriosam delectus fugit tempore voluptatum soluta, rerum necessitatibus, esse perspiciatis cumque totam ratione iusto, ipsa sint repudiandae. Hic, facere.
-          </p>
-        </div>
-
-        <div className='post__comment'>
-          <input className='post__commentUser' type="url" />
-          <input className='post__commentMessage' type="text" placeholder='Write comment as username...' />
-        </div>
-
-      </div>
-
-
-
-    </div>
-  )
-}
-
-
-export default PostUser
+      )
+    }
+    
+    
+    export default PostUser; 
